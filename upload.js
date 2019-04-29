8cf8295188cf68a72c79023498b7d7310ed0418a
Cliff <cliff.crerar@gmail.com>
2019-04-29 19:59:05 +0200

    Install

--- upload.js
+++ upload.js
@@ -1,10 +1,18 @@
-const path = 'path';
-const fs = 'fs';
+const path = require('path');
+const fs = require('fs');
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 
 
-var params = { Bucket: 'bucket', Key: 'key', Body: stream };
-const vid1Stream = fs.read
+var stream = fs.readFileSync('./_oldfiles/PexelsVideos1472013.mp4')
+//console.log('thisBUffer: ', thisBUffer);
 
-s3.upload(params, function (err, data) {
+var params = { Bucket: 'stream-media-aws-infinityarc.tk', Key: 'pagebackgroundvid1.mp4', Body: stream };
+// const vid1Stream = fs.read
+
+var upload = s3.upload(params, finish)
+upload.on('httpUploadProgress', progress => console.log(progress));
+
+function finish(err, data) {
 	console.log(err, data);
-});
+}
